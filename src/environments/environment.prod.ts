8b2b65956297e8e9b29df1753a373486aa4f9d3f
Chaz Mateer <chaz.mateer@timmons.com>
2021-11-11 13:08:00 -0500

    Add header component and refactor with logo and subtitle.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   appVersion: require('../../package.json').version,
   esriApiKey: 'AAPK906c33edfc014bb58ad9eccf7243114d6wSECkZmaVfQ719ZvrMOReTtcPuSjk91PfM6KVbRNojvoO6wUSdgrI0SaKXnRf_E',
-  apiEndpoint: '',
+  apiEndpoint: 'https://ghnl4dzvc0.execute-api.us-east-1.amazonaws.com/Prod',
 };
