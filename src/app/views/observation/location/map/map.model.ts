84b9d9dd806dfd18013672ffa0c6cc2f891d7dc1
Chaz Mateer <chaz.mateer@timmons.com>
2021-12-16 14:48:46 -0500

    Add additional GIS layers.

--- src/app/views/observation/location/map/map.model.ts
+++ src/app/views/observation/location/map/map.model.ts
@@ -10,7 +10,41 @@ export class MapModel {
 
     // PA Counties
     public static FillCountyPaintStyle: any = {
-        'fill-color': '#000',
-        'fill-opacity': 0.5
+        'fill-color': 'rgba(178, 178, 178, 1)',
+        'fill-outline-color': 'rgba(0, 0, 0, 1)'
+    }
+
+    // Regions
+    public static FillRegionPaintStyle: any = {
+        'fill-color': 'rgba(0, 92, 230, 0.4)',
+        'fill-outline-color': 'rgba(0, 92, 230, 1)'
+    }
+    public static LineRegionPaintStyle: any = {
+        'line-color': '#005CE6',
+        'line-width': 2
+    }
+
+    // State Forest
+    public static FillStateForestPaintStyle: any = {
+        'fill-color': 'rgba(76, 115, 0, 1)',
+        'fill-outline-color': 'rgba(76, 115, 0, 1)'
+    }
+
+    // State Park
+    public static FillStateParkPaintStyle: any = {
+        'fill-color': 'rgba(112, 168, 0, 1)',
+        'fill-outline-color': 'rgba(112, 168, 0, 1)'
+    }
+
+    // SGL
+    public static FillSGLPaintStyle: any = {
+        'fill-color': 'rgba(85, 255, 0, 1)',
+        'fill-outline-color': 'rgba(85, 255, 0, 1)'
+    }
+
+    // Roads
+    public static LineRoadsPaintStyle: any = {
+        'line-color': '#000',
+        'line-width': 2
     }
 }
